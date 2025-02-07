325d2189fdf52c0bb2f5dbf0f760b7fb2cd611f3
Fabio Almeida <almeida.fb@gmail.com>
2025-02-07 00:46:43 -0300

    Adding a Redis caching integration

--- AspireSample/AspireSample.AppHost/Program.cs
+++ AspireSample/AspireSample.AppHost/Program.cs
@@ -2,8 +2,12 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.AspireSample_ApiService>("apiservice");
 
+// Add Redis
+var redis = builder.AddRedis("cache");
+
 builder.AddProject<Projects.AspireSample_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
+    .WithExternalHttpEndpoints()    
+    .WithReference(redis)
     .WithReference(apiService)
     .WaitFor(apiService);
 
