bbd622254236e91cc6e2d18100e7ef06ba93993d
Fabio Almeida <almeida.fb@gmail.com>
2025-03-10 21:23:17 -0300

    Add integration test for WeatherForecast endpoint

--- AspireSample/AspireSample.Tests/IntegrationTest1.cs
+++ AspireSample/AspireSample.Tests/IntegrationTest1.cs
@@ -1,3 +1,5 @@
+using System.Net.Http.Json;
+
 namespace AspireSample.Tests.Tests;
 
 public class IntegrationTest1
@@ -33,4 +35,31 @@ public class IntegrationTest1
         // Assert
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     }
+
+    [Fact]
+    public async Task WeatherForecastEndpointReturns5Records()
+    {
+        // Arrange
+        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.AspireSample_AppHost>();
+        appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
+        {
+            clientBuilder.AddStandardResilienceHandler();
+        });
+        // To output logs to the xUnit.net ITestOutputHelper, consider adding a package from https://www.nuget.org/packages?q=xunit+logging
+        await using var app = await appHost.BuildAsync();
+        var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
+        await app.StartAsync();
+
+        // Act
+        var httpClient = app.CreateHttpClient("apiservice");
+        await resourceNotificationService.WaitForResourceAsync("webfrontend", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
+        var response = await httpClient.GetAsync("/weatherforecast");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        var weatherForecasts = await response.Content.ReadFromJsonAsync<WeatherForecast[]>();
+        Assert.Equal(5, weatherForecasts?.Length);
+    }
+
+    record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary);
 }
